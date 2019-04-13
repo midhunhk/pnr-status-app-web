6413f63babaad308a95a7cc2ca41c9a3794ad29c
Midhun Harikumar <midhunhk@gmail.com>
2019-04-13 11:00:29 -0400

    Update erails.js

--- services/erails.js
+++ services/erails.js
@@ -1,28 +1,87 @@
+/**
+ * erails.js
+ * 
+ * The erails service parser
+ * 
+ * @version 1.0
+ * @author midhunhk
+ * 
+ */
 
+/**
+ * Returns the config for invoking this service
+ * 
+ * @param {pnr} pnr 
+ */
 function getConfig(pnr){
-    // TODO append with timestamp
-    const getUrl = `https://data.tripmgt.com/Data.aspx?Action=PNR_STATUS_RR&Data1=${pnr}&t=1554749606601`
+    const ts = new Date().getTime()
+    const getUrl = `https://data.tripmgt.com/Data.aspx?Action=PNR_STATUS_RR&Data1=${pnr}&t=${ts}`
     return {
         method:'get',
         url:getUrl
       }
 }
+
+/**
+ * Parses the invocation of the web response
+ * 
+ * @param {response} response 
+ */
 function parseResponse(response){
     const resultObj = {}
-    resultObj.id = response._id
+    const responseJSON = JSON.parse( response)
+    // resultObj.id = responseJSON._id
     
-    const pnrData = response.PnrData
-
+    const pnrData = responseJSON.PnrData
     resultObj.status = pnrData.status
     resultObj.message = pnrData.message
     
-    const travelData = JSON.parse(response.PnrData + "").data
-    console.log( travelData )
+    const travelData = JSON.parse(responseJSON.PnrData + "").data
 
     resultObj.pnrNo = travelData.pnrNo
-    resultObj.noOfPassenger = travelData.noOfPassenger
+    resultObj.passengersCount = travelData.noOfPassenger
 
-    return resultObj;
+    resultObj.trainDetails = getTrainDetails(travelData)
+    resultObj.travelDetails = getTravelDetails(travelData)
+    
+    resultObj.pasengerDetails = getPassengerDetails(travelData.passengerDetailsDTO, resultObj.passengersCount)
+
+    return resultObj
+}
+
+function getPassengerDetails(passengerData, passengersCount){
+    const passengerDetails = []
+    for(i = 0; i < passengersCount; i++){
+        const responseData = passengerData[i]
+        const passengerInfo = {}
+        passengerInfo.name = "Passenger " + (i +1)
+        passengerInfo.seat = "seat"
+        passengerInfo.status = responseData.seatStts
+        passengerInfo.quota = response.quotaCode
+        passengerDetails.push(passengerInfo)
+    }
+    return passengerDetails
+}
+
+function getTrainDetails(pnrData){
+    const trainDetails = {}
+    trainDetails.trainNo = pnrData.trainNum
+    trainDetails.trainName = pnrData.trainName
+    trainDetails.trainFrom = pnrData.stationFrom
+    trainDetails.trainTo = pnrData.stationTo
+    return trainDetails
+}
+
+function getTravelDetails(pnrData){
+    const travelDetails = {}
+    travelDetails.boardingPoint = pnrData.boardingPoint
+    travelDetails.reservedUpto = pnrData.reservationUpTo
+    travelDetails.travelDate = pnrData.departureDate
+    travelDetails.travelDateString = pnrData.departureDate
+    travelDetails.ticketClass = pnrData.journeyClass
+    travelDetails.bookingStatus = pnrData.chartStts
+    travelDetails.currentStatus = pnrData.chartStts
+    return travelDetails
 }
 
 module.exports = {
